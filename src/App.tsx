6ebe278e35107e0296f0ffb201a790d7e2d9c05c
Rayner <44660318+kazooie20@users.noreply.github.com>
2023-02-17 14:40:35 +0800

    Integrate Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{FC, ReactElement} from 'react';
+import {ThemeProvider, CssBaseline} from '@mui/material';
+import {customTheme} from './theme/customTheme';
 
-
-function App() {
+const App: FC = (): ReactElement => {
   return (
-    <h1>Hello World</h1>
-  );
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
 }
 
 export default App;
